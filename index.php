f8a35df2df06f1d34a5ebec16ef68a162d7236a1
Manuel González R <mgrivera@gmail.com>
2013-03-21 13:27:02 -0600

    Added CSStyles

--- index.php
+++ index.php
@@ -1,12 +1,174 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <title>The Blue Marble project</title>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+        <meta name="keywords" content="Blue Marble NASA SpaceApps Challenge Image"/>
+        <meta name="description" content="{m_descri}"/>
+        <meta name="author" content="{m_author}"/>
+        <meta name="robots" content="{m_robots}"/>        
+        <link rel="shortcut icon" type="image/x-icon" href="media/favicon.ico">        
+        <link type="text/css" rel="stylesheet" href="css/bluemarble.css"/>        
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+         <div id="bluemarble_top_section">	
+            <div class="bluemarble_container">
+                <div id="bluemarble_header">
+                    <div id="bluemarble_logo_area">
+                        <h2>The Blue Marble</h2>
+                        <p>A blue pale dot in the space</p>
+                    </div>               
+                    <div id="bluemarble_header_right">
+                        <h2>Morbi malesuada velit</h2>
+                        <p>
+                            This is a standards-compliant CSS template provided by 
+                            <a href="http://www.bluemarble.com" target="_parent">TemplateMo.com</a> for free of charge.
+                        </p>
+                    </div>
+                    <div class="cleaner"></div>                
+                    <div id="bluemarble_menu">
+                        <ul>
+                            <li class="current">
+                               <a href="#">home</a>
+                                <p>start here</p>
+                            </li>
+                            <li>
+                                <a href="http://www.bluemarble.com" target="_parent">portfolio</a>
+                                <p> our work</p>
+                            </li>
+                            <li>
+                                <a href="#">about</a>
+                                <p>who we are</p>
+                            </li>
+                            <li>
+                                <a href="#">contact</a>
+                                <p>get in touch</p>
+                            </li>
+                            <li>
+                                <a href="#">testing</a>
+                                <p>test</p>
+                            </li>
+                            <li>
+                                <a href="#">testing</a>
+                                <p>test</p>
+                            </li>
+                            <li>
+                                <a href="#">testing</a>
+                                <p>test</p>
+                            </li>
+                            <li>
+                                <a href="#">testing</a>
+                                <p>test</p>
+                            </li>
+                            <li class="last">
+                                <a href="#">archives</a>
+                                <p>see more</p>
+                            </li>
+                           
+                        </ul> 
+                    </div>
+                </div>
+            </div><!-- End Of Container  -->        
+        </div><!-- End Of Top Section -->   
+        <div id="bluemarble_middle_section">  
+            <div class="bluemarble_container">
+                <div id="bluemarble_content">
+                    <div class="bluemarble_3_col left_section">
+                        <h1>news &amp; events</h1>
+                        <h2>Aenean cursmaecenas</h2>
+                        <p>
+                           Praesent dapibus, neque id cursus faucibus, tortor 
+                           neque egestas augue, eu vulputate magna eros eu erat. 
+                           Aliquam erat volutpat. Nam dui mi, tincidunt quis, 
+                           accumsan porttitor, facilisis luctus, metus. 
+                           <a href="#">Read More</a>
+                        </p>                   
+                        <h2>Morbi est est</h2>
+                        <p>
+                            Sed adipiscing ornare risus. Morbi est est, blandit 
+                            sit amet, sagittis vel, euismod vel, velit. 
+                            Pellentesque egestas sem. Suspendisse commodo 
+                            ullamcorper magna. <a href="#">Read More</a>
+                        </p>                   
+                        <h2>Dapibu dignissim</h2>
+                        <p>
+                            Validate <a href="http://validator.w3.org/check?uri=referer">XHTML</a> 
+                            and <a href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a> codes. 
+                            Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. 
+                            Aenean dignissim pellentesque felis. <a href="#">Read More</a>
+                        </p>
+            </div>               
+            <div class="bluemarble_3_col middle">
+                <h1>our portfolio</h1>                   
+                    <a href="http://www.bluemarble.com/preview/bluemarble_150_creative_idea" target="_blank"><img src="images/bluemarble_img_2.png" alt="creative web template" border="0" class="mid_img" /></a>                    
+                    <a href="http://www.bluemarble.com/preview/bluemarble_151_grunge" target="_blank"><img src="images/bluemarble_img_3.png" alt="grunge css template" border="0" class="mid_img" /></a>
+                    <a href="#"><img src="images/bluemarble_img_4.png" alt="free css template" border="0" class="mid_img" /></a>  	
+                    <br /><a href="#"><strong>View All</strong></a>
+            </div>
+               
+            <div class="bluemarble_3_col right_section">
+                <h1>about us</h1>
+                <img class="image" src="images/bluemarble_img_1.png" alt="Icon" />
+                <p>Duis vitae velit sed dui malesuada dignissim. Donec mollis aliquet ligula. Maecenas adipiscing elementum ipsum. Pellentesque vitae magna. Sed nec est. Suspendisse a nibh tristique justo rhoncus volutpat.</p>                   
+                <ul class="bluemarble_list">
+                    <li><a href="#">Quisque in diam a justo condimentum</a></li>
+                    <li><a href="#">Fusce urna tortor, consequat eu stie</a></li>
+                    <li><a href="http://www.bluemarble.com" target="_parent">Download Free Website Templates</a></li>
+                </ul>               
+            </div>
+            <div class="cleaner"></div>
+        </div>
+    </div><!-- End Of Container  -->    
+</div><!-- End Of Middle Section  -->   
+  
+  <div id="bluemarble_buttom_section">
+<div class="bluemarble_container">
+<div class="bluemarble_bottom_panel">
+            <div class="bluemarble_3_col bottom_left">
+                <h2>privacy policy</h2>
+                <p>Pellentesque fermentum dolor. Aliquam quam lectus, facilisis auctor, ultrices ut, elementum vulputate, nunc.</p>
+                <div id="bluemarble_footer">                    
+                    <div id="bluemarble_footer_top"></div>
+                    <div id="bluemarble_footer_mid">
+                        Copyright &copy; 2024 <a href="#">Your Company Name<br />
+                        </a>Designed by <a href="http://www.bluemarble.com/page/1" target="_parent">Free CSS Templates</a>
+                    </div>
+                    <div id="bluemarble_footer_bottom"></div>
+                </div>
+            </div>               
+            <div class="bluemarble_3_col bottmo_middle">
+                <h2>useful links</h2>
+                <ul class="bluemarble_list bullet_arrow">
+                    <li><a href="#">Cras ornare tris tique</a></li>
+                    <li><a href="#">Vivamus vestibulum nulla </a></li>
+                    <li><a href="#">Praesent placerat risus</a></li>
+                    <li><a href="#">Fusce pellentesque sus</a></li>
+                    <li><a href="#">Integer vitae libero</a></li>
+                </ul>                   
+                <ul class="bluemarble_list bullet_arrow">
+                    <li><a href="#">Vestibulum commodo felis</a></li>
+                    <li><a href="#">Ut aliquam sollicitudin</a></li>
+                    <li><a href="#">Cras iaculis ultricies</a></li>
+                    <li><a href="#">Sus risus nulla tique</a></li>
+                    <li><a href="http://www.bluemarble.com" target="_blank">Free CSS Templates</a></li>
+                </ul>
+            </div>               
+            <div class="bluemarble_3_col bottom_right">
+                <div class="bluemarble_newsletter">
+                    <h4>Newsletter</h4>
+                    <p>Enter your email address below.</p>
+                    <form action="#" method="post">
+                        <label>Email : </label>
+                        <input type="text" value="" name="name" class="field"  title="E-mail" onfocus="clearText(this)" onblur="clearText(this)" />
+                        <input type="submit" name="search" value = "Subscribe" alt="Search" class="button" title="Subscribe" />
+                        <div class="cleaner"></div>
+                    </form>
+                </div>
+            </div>               
+            <div class="cleaner"></div>
+        </div>
+    </div><!-- End Of Container  -->   
+  </div><!-- End Of Bottom Section  -->   
+        
     </body>
 </html>
