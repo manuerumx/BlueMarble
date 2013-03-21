ad391cea3dbe8411d746175e7476c202ca4419a2
Manuel González R <mgrivera@gmail.com>
2013-03-21 16:43:01 -0600

    Css improved

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
-<!DOCTYPE html>
+<!DOCTYPE html> 
 <html>
     <head>
-        <title>The Blue Marble project</title>
+        <title>The Blue Marble Project</title>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <meta name="keywords" content="Blue Marble NASA SpaceApps Challenge Image"/>
         <meta name="description" content="{m_descri}"/>
@@ -10,61 +10,43 @@
         <link rel="shortcut icon" type="image/x-icon" href="media/favicon.ico">        
         <link type="text/css" rel="stylesheet" href="css/bluemarble.css"/>        
     </head>
-    <body>
-         <div id="bluemarble_top_section">	
-            <div class="bluemarble_container">
+    <body>        
+        <div id="bluemarble_top_section">
+            <div class="bluemarble_container">                
+                <div id="blumarble_toolbar">Language: English</div>
                 <div id="bluemarble_header">
                     <div id="bluemarble_logo_area">
                         <h2>The Blue Marble</h2>
-                        <p>A blue pale dot in the space</p>
+                        <p>Look again at that dot. That's here. That's home.</p>
                     </div>               
-                    <div id="bluemarble_header_right">
-                        <h2>Morbi malesuada velit</h2>
-                        <p>
-                            This is a standards-compliant CSS template provided by 
-                            <a href="http://www.bluemarble.com" target="_parent">TemplateMo.com</a> for free of charge.
-                        </p>
-                    </div>
+                    <div id="bluemarble_header_right"></div>
                     <div class="cleaner"></div>                
                     <div id="bluemarble_menu">
                         <ul>
                             <li class="current">
-                               <a href="#">home</a>
-                                <p>start here</p>
+                               <a href="#">Home</a>
+                                <p>Start here</p>
+                            </li>                                                       
+                            <li>
+                                <a href="#">News</a>
+                                <p>News &amp; Events</p>
                             </li>
                             <li>
-                                <a href="http://www.bluemarble.com" target="_parent">portfolio</a>
-                                <p> our work</p>
+                                <a href="#">The Blue</a>
+                                <p>Marble</p>
                             </li>
                             <li>
-                                <a href="#">about</a>
-                                <p>who we are</p>
-                            </li>
+                                <a href="/BlueMarble" target="_parent">Archives</a>
+                                <p>Discover our home</p>
+                            </li> 
                             <li>
-                                <a href="#">contact</a>
-                                <p>get in touch</p>
-                            </li>
-                            <li>
-                                <a href="#">testing</a>
-                                <p>test</p>
-                            </li>
-                            <li>
-                                <a href="#">testing</a>
-                                <p>test</p>
-                            </li>
-                            <li>
-                                <a href="#">testing</a>
-                                <p>test</p>
-                            </li>
-                            <li>
-                                <a href="#">testing</a>
-                                <p>test</p>
+                                <a href="#">Help Us</a>
+                                <p>Get involved</p>
                             </li>
                             <li class="last">
-                                <a href="#">archives</a>
-                                <p>see more</p>
-                            </li>
-                           
+                                <a href="#">Login</a>
+                                <p>Register</p>
+                            </li>                            
                         </ul> 
                     </div>
                 </div>
@@ -102,13 +84,12 @@
                 <h1>our portfolio</h1>                   
                     <a href="http://www.bluemarble.com/preview/bluemarble_150_creative_idea" target="_blank"><img src="images/bluemarble_img_2.png" alt="creative web template" border="0" class="mid_img" /></a>                    
                     <a href="http://www.bluemarble.com/preview/bluemarble_151_grunge" target="_blank"><img src="images/bluemarble_img_3.png" alt="grunge css template" border="0" class="mid_img" /></a>
-                    <a href="#"><img src="images/bluemarble_img_4.png" alt="free css template" border="0" class="mid_img" /></a>  	
+                    <a href="#"><img src="media/page/bluemarble_img_4.png" alt="free css template" border="0" class="mid_img" /></a>  	
                     <br /><a href="#"><strong>View All</strong></a>
             </div>
                
             <div class="bluemarble_3_col right_section">
-                <h1>about us</h1>
-                <img class="image" src="images/bluemarble_img_1.png" alt="Icon" />
+                <h1>about us</h1>                
                 <p>Duis vitae velit sed dui malesuada dignissim. Donec mollis aliquet ligula. Maecenas adipiscing elementum ipsum. Pellentesque vitae magna. Sed nec est. Suspendisse a nibh tristique justo rhoncus volutpat.</p>                   
                 <ul class="bluemarble_list">
                     <li><a href="#">Quisque in diam a justo condimentum</a></li>
@@ -125,19 +106,14 @@
 <div class="bluemarble_container">
 <div class="bluemarble_bottom_panel">
             <div class="bluemarble_3_col bottom_left">
-                <h2>privacy policy</h2>
-                <p>Pellentesque fermentum dolor. Aliquam quam lectus, facilisis auctor, ultrices ut, elementum vulputate, nunc.</p>
-                <div id="bluemarble_footer">                    
-                    <div id="bluemarble_footer_top"></div>
-                    <div id="bluemarble_footer_mid">
-                        Copyright &copy; 2024 <a href="#">Your Company Name<br />
-                        </a>Designed by <a href="http://www.bluemarble.com/page/1" target="_parent">Free CSS Templates</a>
-                    </div>
-                    <div id="bluemarble_footer_bottom"></div>
+                <div class="nasa">
+                <a href="http://nasa.gov/" target="_blank">
+                    <img src="media/page/nasa_logo.png" alt="NASA logo" Title="NASA"/>                
+                </a>
                 </div>
             </div>               
-            <div class="bluemarble_3_col bottmo_middle">
-                <h2>useful links</h2>
+            <div class="bluemarble_3_col bottom_middle">
+                <h2>Useful Links</h2>
                 <ul class="bluemarble_list bullet_arrow">
                     <li><a href="#">Cras ornare tris tique</a></li>
                     <li><a href="#">Vivamus vestibulum nulla </a></li>
@@ -154,19 +130,15 @@
                 </ul>
             </div>               
             <div class="bluemarble_3_col bottom_right">
-                <div class="bluemarble_newsletter">
-                    <h4>Newsletter</h4>
-                    <p>Enter your email address below.</p>
-                    <form action="#" method="post">
-                        <label>Email : </label>
-                        <input type="text" value="" name="name" class="field"  title="E-mail" onfocus="clearText(this)" onblur="clearText(this)" />
-                        <input type="submit" name="search" value = "Subscribe" alt="Search" class="button" title="Subscribe" />
-                        <div class="cleaner"></div>
-                    </form>
+                <div class="spaceapps">
+                <a href="http://spaceappschallenge.org/" target="_blank">
+                    <img src="media/page/spaceappschallenge-125x129.png" alt="SpaceAppsChallenge logo" Title="Space Apps Challenge 2013"/>                
+                </a>
                 </div>
-            </div>               
+            </div>                
             <div class="cleaner"></div>
         </div>
+        <div id="bluemarble_copy"></div>                
     </div><!-- End Of Container  -->   
   </div><!-- End Of Bottom Section  -->   
         
