1b4e504ba8bc27d266efe76f0e26643185d894ed
Manuel González R <manuel@dsindigo.com>
2013-05-21 16:41:31 -0500

    Update DB file

--- db/BlueMarble.sql
+++ db/BlueMarble.sql
@@ -1,102 +1,52 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+delimiter $$
 
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+CREATE TABLE `bm_user` (
+  `idbm_user` int(11) NOT NULL AUTO_INCREMENT,
+  `bm_username` varchar(30) COLLATE utf8_bin NOT NULL,
+  `bm_email` varchar(50) COLLATE utf8_bin NOT NULL,
+  `bm_pass` varchar(200) COLLATE utf8_bin NOT NULL,
+  `bm_lang` varchar(5) COLLATE utf8_bin NOT NULL DEFAULT 'en_US',
+  `bm_timezone` varchar(50) COLLATE utf8_bin DEFAULT NULL,
+  `bm_lastlog` datetime DEFAULT NULL,
+  `bm_membersince` datetime DEFAULT NULL,
+  PRIMARY KEY (`idbm_user`),
+  UNIQUE KEY `bm_username_UNIQUE` (`bm_username`),
+  UNIQUE KEY `idbm_user_UNIQUE` (`idbm_user`),
+  UNIQUE KEY `bm_email_UNIQUE` (`bm_email`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin$$
 
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+delimiter $$
 
-
-
-CREATE SCHEMA IF NOT EXISTS `BlueMarble` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin ;
-
-
-
-USE `BlueMarble`;
-
-
-
-CREATE  TABLE IF NOT EXISTS `BlueMarble`.`bm_user` (
-
-  `idbm_user` INT(11) NOT NULL AUTO_INCREMENT ,
-
-  `bm_username` VARCHAR(30) NOT NULL ,
-
-  `bm_email` VARCHAR(50) NOT NULL ,
-
-  `bm_pass` VARCHAR(200) NOT NULL ,
-
-  `bm_lang` VARCHAR(5) NOT NULL DEFAULT 'en_US' ,
-
-  `bm_timezone` VARCHAR(50) NULL DEFAULT NULL ,
-
-  `bm_lastlog` DATETIME NULL DEFAULT NULL ,
-
-  `bm_membersince` DATETIME NULL DEFAULT NULL ,
-
-  PRIMARY KEY (`idbm_user`) ,
-
-  UNIQUE INDEX `bm_username_UNIQUE` (`bm_username` ASC) ,
-
-  UNIQUE INDEX `idbm_user_UNIQUE` (`idbm_user` ASC) ,
-
-  UNIQUE INDEX `bm_email_UNIQUE` (`bm_email` ASC) )
-
-ENGINE = InnoDB
-
-DEFAULT CHARACTER SET = utf8
-
-COLLATE = utf8_bin;
+CREATE TABLE `images_dataset` (
+  `Mission` varchar(20) COLLATE utf8_bin NOT NULL,
+  `Roll` varchar(15) COLLATE utf8_bin NOT NULL,
+  `Frame` int(11) NOT NULL,
+  `Width` int(11) NOT NULL,
+  `Height` int(11) NOT NULL,
+  `Filesize` int(11) NOT NULL,
+  `CLDP` int(11) DEFAULT NULL,
+  `Lat` double NOT NULL,
+  `Lon` double NOT NULL,
+  `Geon` varchar(100) COLLATE utf8_bin DEFAULT NULL,
+  `Feat` varchar(200) COLLATE utf8_bin DEFAULT NULL,
+  `Url` varchar(200) COLLATE utf8_bin NOT NULL,
+  KEY `Idx_Mission` (`Mission`,`Roll`,`Frame`),
+  KEY `Idx_Coord` (`Lat`,`Lon`),
+  KEY `Idx_Type` (`Width`,`Height`,`Filesize`),
+  KEY `Idx_Url` (`Url`),
+  KEY `Idx_Feat` (`Feat`,`Geon`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin$$
 
 
 
-CREATE  TABLE IF NOT EXISTS `BlueMarble`.`Images_Dataset` (
+delimiter $$
 
-  `Mission` VARCHAR(20) NOT NULL ,
+CREATE TABLE `iss_dataset` (
+  `Mission` varchar(45) COLLATE utf8_bin NOT NULL,
+  `Latitude` double NOT NULL,
+  `Longitude` double NOT NULL,
+  PRIMARY KEY (`Mission`),
+  KEY `COORD` (`Latitude`,`Longitude`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin$$
 
-  `Roll` VARCHAR(15) NOT NULL ,
-
-  `Frame` INT(11) NOT NULL ,
-
-  `Width` INT(11) NOT NULL ,
-
-  `Height` INT(11) NOT NULL ,
-
-  `Filesize` INT(11) NOT NULL ,
-
-  `CLDP` INT(11) NULL DEFAULT NULL ,
-
-  `Lat` DECIMAL(6) NOT NULL ,
-
-  `Lon` DECIMAL(6) NOT NULL ,
-
-  `Geon` VARCHAR(100) NULL DEFAULT NULL ,
-
-  `Feat` VARCHAR(200) NULL DEFAULT NULL ,
-
-  `Url` VARCHAR(200) NOT NULL ,
-
-  INDEX `Idx_Mission` (`Mission` ASC, `Roll` ASC, `Frame` ASC) ,
-
-  INDEX `Idx_Coord` (`Lat` ASC, `Lon` ASC) ,
-
-  INDEX `Idx_Type` (`Width` ASC, `Height` ASC, `Filesize` ASC) ,
-
-  INDEX `Idx_Url` (`Url` ASC, `Feat` ASC, `Geon` ASC) )
-
-ENGINE = InnoDB
-
-DEFAULT CHARACTER SET = utf8
-
-COLLATE = utf8_bin
-
-COMMENT = '			';
-
-
-
-
-
-SET SQL_MODE=@OLD_SQL_MODE;
-
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
